4e71655e27550991c8c7986f823e9ddb4b769eb2
Chiveygnome <ernie_wheaton@knights.ucf.edu>
2017-05-08 23:09:33 -0400

    GUI fixes

--- Main/MainWindow.py
+++ Main/MainWindow.py
@@ -2,34 +2,55 @@ import sys
 from PySide import QtCore, QtGui
 
 """
-    This is the main window of the program.  As of 5/7/17, this is broken.
-    None of the widgets are showing.  May be due to incorrect sizing, however
-    I am unsure - Ernie
+    This is the main window of the program.  As of 5/7/17, this has a few bugs.
+    The qtextbox doesn't move the cursor correctly, kind of breaking the text input.
 """
-
+MAX_LENGTH = 140
 class MainWidget(QtGui.QWidget):
-    def __init__(self, *args):
-        super(MainWidget, self).__init__(*args)
-        self.layout = QtGui.QVBoxLayout()
-        self.setupTextBox()
-        self.setupButtons()
+    def __init__(self, parent=None,*args, **kwargs):
+        super(MainWidget, self).__init__(parent,*args, **kwargs)
+        self.length = 0
+        layout = QtGui.QVBoxLayout()
+        self.setupTextBox(layout)
+        self.setupButtons(layout)
+        self.setLayout(layout)
         self.show()
 
-    def setupTextBox(self):
-        self.box = QtGui.QPlainTextEdit()
+    def setupTextBox(self, layout):
+        self.box = QtGui.QPlainTextEdit(parent=self)
         self.box.setLineWidth(80)
         self.box.setLineWrapMode(QtGui.QPlainTextEdit.LineWrapMode.WidgetWidth)
+        self.box.setFocus()
+        
         self.box.setTabChangesFocus(True)
-        self.layout.addWidget(self.box)
+        self.box.textChanged.connect(self.onTextChanged)
+        layout.addWidget(self.box)
         
 
-    def setupButtons(self):
-        self.sendButton = QtGui.QPushButton()
+    def setupButtons(self, layout):
+        hlayout = QtGui.QHBoxLayout()
+        self.sendButton = QtGui.QPushButton(parent=self)
         self.sendButton.setText("Post Question")
         self.sendButton.setFixedSize(QtCore.QSize(100,50))
         self.sendButton.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
         self.sendButton.adjustSize()
-        self.layout.addWidget(self.sendButton)
+        hlayout.addWidget(self.sendButton)
+        self.charsLeftLine = QtGui.QLineEdit()
+        self.charsLeftLine.setReadOnly(True)
+        self.charsLeftLine.setFixedSize(QtCore.QSize(100,50))
+        self.charsLeftLine.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        self.charsLeftLine.setText("{0} characters left".format(MAX_LENGTH))
+        self.charsLeftLine.setFrame(False)
+        self.charsLeftLine.setBackgroundRole(QtGui.QPalette.ColorRole.Midlight)
+        hlayout.addWidget(self.charsLeftLine)
+        layout.addLayout(hlayout)
+
+    # This is a slot for the signal textChanged in the QTextBox class
+    def onTextChanged(self):
+        length = len(self.box.toPlainText())
+        if( length > MAX_LENGTH ):
+            self.box.setPlainText(self.box.toPlainText()[0:MAX_LENGTH])
+            self.box.textCursor().movePosition(QtGui.QTextCursor.End, QtGui.QTextCursor.MoveAnchor)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
